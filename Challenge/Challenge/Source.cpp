c9ef03a58cfc0d39a407d4f6d8fa172ce1d7d885
Nakanokpol Sombattheera <nakanokpol49066@gmail.com>
2020-08-16 20:42:42 +0700

    *-*

---
+++ Challenge/Challenge/Source.cpp
@@ -0,0 +1,53 @@
+//Challenge_loop_if_basicmath
+
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	double Avr, d, sd, a, b, c, sum = 0, e= 0;
+	scanf_s("%lf", &a);
+	scanf_s("%lf", &b);
+
+	if (a > b)
+	{
+		c = a - b +1;
+		for (int i = a; i>=b; i--)
+		{
+			sum += i;
+			printf("%lf ", i);
+		}
+	}
+
+	else if (a<b)
+	{
+		c = b - a +1 ;
+		for (int i=a; i<=b; i++)
+		{
+			sum += i;
+		}
+	}
+
+	Avr = sum / c;
+	if (a > b)
+	{
+		for (int i= a; i>=b ; i--)
+		{
+			d = pow(i-Avr,2);
+			e += d;
+		}
+	}
+	else if (a < b)
+	{
+		for (int i= a; i <= b; i++)
+		{
+			d = pow(i - Avr, 2);
+			e += d;
+		}
+	}
+
+	sd = sqrt(e /(c-1));
+
+	printf("\nAverage = %lf", Avr);
+	printf("\nS.D. = %lf", sd);
+}
